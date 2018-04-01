a34f35aa524330d29a8f9ed08d82f6cb3be8a149
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2018-03-31 19:17:45 -0700

    moved the login function out of util

--- util/index.js
+++ util/index.js
@@ -1,6 +1,8 @@
 const dotenv = require('dotenv');
 const isDocker = require('is-docker');
 
+const login = require('./login');
+
 module.exports = {
     getConsoleLink: (region, service, subpath) =>
         `https://console.aws.amazon.com/${service}/home?region=${region}#${subpath}`,
@@ -9,26 +11,9 @@ module.exports = {
             dotenv.config();
         }
     },
-    login: async (browser, url) => {
-        const { AWS_ACCOUNT, IAM_USERNAME, IAM_PASSWORD } = process.env;
-        const page = await browser.newPage();
-
-        // AWS account id or alias page
-        await page.goto(url);
-        await page.waitForSelector('#resolver_container #resolving_input');
-        await page.type('#resolver_container #resolving_input', AWS_ACCOUNT);
-        await page.click('button#next_button');
-
-        // IAM account login page
-        await page.waitForSelector('#accountFields #username');
-        await page.type('#accountFields #username', IAM_USERNAME);
-        await page.type('#accountFields #password', IAM_PASSWORD);
-        await page.click('a#signin_button');
-
-        return page;
-    },
     screenshot: async (page, path) => (
         process.env.DEBUG === 'true' ? page.screenshot({ path: path, fullPage: true }) : false
     ),
-    isFargateRegion: region => ['us-east-1'].includes(region)
+    isFargateRegion: region => ['us-east-1'].includes(region),
+    login: login
 };
