f90dc0aa002c4998684ff1b8a37c4321be026db5
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2018-04-02 07:29:57 -0700

    started adding tests for the cluster creation

--- src/clusters.spec.js
+++ src/clusters.spec.js
@@ -6,7 +6,9 @@ const {
     getConsoleLink,
     setupEnvironment,
     login,
-    screenshot
+    screenshot,
+    isFargateRegion,
+    addToManifest
 } = require('../util');
 
 let browser;
@@ -29,11 +31,30 @@ describe('clusters page', () => {
     test('shows up when navigated to', async () => {
         const page = await login(browser, consoleLink);
 
-        // clusters page
         await page.waitForSelector('awsui-button#create-cluster-button');
         const content = await page.content();
 
         await screenshot(page, path.resolve(process.cwd(), './artifacts/clusters-page.png'));
+
+        expect(content.length).not.toBe(0);
+    });
+
+    test('creates an empty cluster in non-fargate region', async () => {
+        if (isFargateRegion(process.env.REGION)) {
+            return;
+        }
+
+        const page = await login(browser, consoleLink);
+
+        // clusters page
+        await page.waitForSelector('awsui-button#create-cluster-button');
+        await page.click('awsui-button#create-cluster-button');
+
+        // cluster type page
+        await page.waitForSelector('aws-button[primary-button]');
+        const content = await page.content();
+
+        await screenshot(page, path.resolve(process.cwd(), './artifacts/empty-ec2-cluster.png'));
 
         expect(content.length).not.toBe(0);
     });
