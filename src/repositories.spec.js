2e38cf7cbe680c1ebb5655c52cce7abca4752be1
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2018-04-10 20:40:37 -0700

    added a test for creating repositories

--- src/repositories.spec.js
+++ src/repositories.spec.js
@@ -1,20 +1,19 @@
 const path = require('path');
 const puppeteer = require('puppeteer');
-// const { hacker } = require('faker');
+const { hacker } = require('faker');
 
 const {
     getConsoleLink,
     setupEnvironment,
     login,
-    screenshot
-    // isFargateRegion,
-    // addToManifest
+    screenshot,
+    addToManifest
 } = require('../util');
 
 let browser;
 let consoleLink;
 
-jest.setTimeout(600 * 1000);
+jest.setTimeout(300 * 1000);
 
 beforeEach(async () => {
     setupEnvironment();
@@ -35,6 +34,29 @@ describe('repositories page', () => {
         const content = await page.content();
 
         await screenshot(page, path.resolve(process.cwd(), './artifacts/repositories-page.png'));
+
+        expect(content.length).not.toBe(0);
+    });
+
+    test('finishes out the flow', async () => {
+        const repoName = `repo-${hacker.verb()}`;
+        const page = await login(browser, consoleLink);
+
+        // repositories page
+        await page.waitForSelector('awsui-button#create-repository-button');
+        await page.click('awsui-button#create-repository-button');
+
+        // create repository page
+        await page.waitForSelector('awsui-textfield[ng-model="wizardValues.repositoryName"]');
+        await page.type('awsui-textfield[ng-model="wizardValues.repositoryName"] input', repoName);
+        await page.click('aws-button[primary-button]');
+
+        // push image page
+        await page.waitForSelector('awsui-alert[type="success"]');
+        const content = await page.content();
+
+        await addToManifest('repository', repoName);
+        await screenshot(page, path.resolve(process.cwd(), './artifacts/repository.png'));
 
         expect(content.length).not.toBe(0);
     });
