35fe48a979e1683e78f1d29dd8c7e98b9480dbaf
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2018-04-12 21:39:02 -0700

    added a test to build a taskdef

---
+++ src/task-definition.spec.js
@@ -0,0 +1,77 @@
+const path = require('path');
+const puppeteer = require('puppeteer');
+
+const taskDefSample = require('../util/sample-taskdef.json');
+
+const {
+    getConsoleLink,
+    setupEnvironment,
+    login,
+    screenshot,
+    isFargateRegion,
+    addToManifest
+} = require('../util');
+
+let browser;
+let consoleLink;
+
+jest.setTimeout(300 * 1000);
+
+beforeEach(async () => {
+    setupEnvironment();
+    browser = await puppeteer.launch({
+        args: ['--no-sandbox']
+    });
+
+    consoleLink = getConsoleLink(process.env.REGION, 'ecs', '/taskDefinitions');
+});
+
+afterEach(() => browser.close());
+
+describe('taskdef page', () => {
+    test('shows up when navigated to', async () => {
+        const page = await login(browser, consoleLink);
+
+        await page.waitForSelector('awsui-button#create-new-task-def-button');
+        const content = await page.content();
+
+        await screenshot(page, path.resolve(process.cwd(), './artifacts/taskdef-page.png'));
+
+        expect(content.length).not.toBe(0);
+    });
+
+    test('creates a new task def revision in non-fargate region', async () => {
+        if (isFargateRegion(process.env.REGION)) {
+            return;
+        }
+
+        const page = await login(browser, consoleLink);
+
+        // taskdef list page
+        await page.waitForSelector('awsui-button#create-new-task-def-button');
+        await page.click('awsui-button#create-new-task-def-button');
+
+        // taskdef create page
+        await page.waitForSelector('awsui-button[text="Configure via JSON"]');
+        await page.click('awsui-button[text="Configure via JSON"]');
+
+        // taskdef json panel
+        await page.waitFor(() => {
+            document.querySelector('awsui-textarea.json-content-textarea textarea').value = '';
+            return document.querySelector('awsui-textarea.json-content-textarea textarea').value === '';
+        });
+        await page.type('awsui-textarea.json-content-textarea textarea', JSON.stringify(taskDefSample, null, 2));
+        await page.click('awsui-button[text="Save"]');
+        await page.waitFor(1000); // pretty sure we disable the create button for some reason
+        await page.click('awsui-button[text="Create"]');
+
+        // results page
+        await page.waitForSelector('div.view-task-definition');
+        const success = await page.$$('awsui-alert[type="success"]');
+
+        await addToManifest('taskDefinition', 'ecs-tester-taskdef');
+        await screenshot(page, path.resolve(process.cwd(), './artifacts/created-taskdef.png'));
+
+        expect(success).toHaveLength(1);
+    });
+});
