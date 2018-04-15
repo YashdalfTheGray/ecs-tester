b17386e0b72601bbf38cad7d453762963288729d
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2018-04-14 19:52:29 -0700

    added a task definition create for fargate regions

--- src/task-definition.spec.js
+++ src/task-definition.spec.js
@@ -70,8 +70,50 @@ describe('taskdef page', () => {
         const success = await page.$$('awsui-alert[type="success"]');
 
         await addToManifest('taskDefinition', 'ecs-tester-taskdef');
-        await screenshot(page, path.resolve(process.cwd(), './artifacts/created-taskdef.png'));
+        await screenshot(page, path.resolve(process.cwd(), './artifacts/created-ec2-taskdef.png'));
 
         expect(success).toHaveLength(1);
+    });
+
+    test('creates a new task def revision in fargate region', async () => {
+        if (!isFargateRegion(process.env.REGION)) {
+            return;
+        }
+
+        const page = await login(browser, consoleLink);
+
+        // taskdef list page
+        await page.waitForSelector('awsui-button#create-new-task-def-button');
+        await page.click('awsui-button#create-new-task-def-button');
+
+        // taskded type page
+        await page.waitForSelector('#create-task-def-fargate-card');
+        await page.click('aws-button[primary-button]');
+
+        // taskdef create page
+        await page.waitForSelector('awsui-button[text="Configure via JSON"]');
+        await page.click('awsui-button[text="Configure via JSON"]');
+
+        // taskdef json panel
+        await page.waitFor(() => {
+            document.querySelector('awsui-textarea.json-content-textarea textarea').value = '';
+            return document.querySelector('awsui-textarea.json-content-textarea textarea').value === '';
+        });
+        await page.type('awsui-textarea.json-content-textarea textarea', JSON.stringify(taskDefSample, null, 2));
+        await page.click('awsui-button[text="Save"]');
+        await page.waitFor(1000); // pretty sure we disable the create button for some reason
+        await page.click('aws-button[primary-button]');
+
+        // results page
+        await page.waitFor(
+            () => !document.querySelectorAll('awsui-alert[type="info"]').length,
+            { timeout: 5 * 1000 }
+        );
+        const errors = await page.$$('awsui-alert[type="error"]');
+
+        await addToManifest('taskDefinition', 'ecs-tester-taskdef');
+        await screenshot(page, path.resolve(process.cwd(), './artifacts/created-fargate-taskdef.png'));
+
+        expect(errors).toHaveLength(0);
     });
 });
