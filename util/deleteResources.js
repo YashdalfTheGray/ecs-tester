4d4ee3513f5d30268408cb39943a926b8a8c8fc2
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2018-04-21 17:24:54 -0700

    didn't save, huh?

--- util/deleteResources.js
+++ util/deleteResources.js
@@ -3,31 +3,25 @@ const { ECR, ECS } = require('aws-sdk');
 module.exports = async manifest => Promise.all(manifest.resources.map((r) => {
     console.log(`Deleting ${r.type} ${r.name}`);
     switch (r.type) {
-    case 'taskDefinition':
-        return deleteTaskDefinition(r);
     case 'cluster':
         return deleteCluster(r);
     case 'repository':
         return deleteRespository(r);
+    case 'taskDefinition':
     default:
         return Promise.resolve();
     }
 }));
 
-const deleteTaskDefinition = async (r) => {
+const deleteCluster = async (r) => {
     const ecs = new ECS({
         accessKeyId: process.env.ACCESS_KEY_ID,
         secretAccessKey: process.env.SECRET_ACCESS_KEY,
         region: process.env.REGION
     });
 
-    const response = await ecs.describeTaskDefinition({
-
-    }).promise();
     return Promise.resolve(r);
 };
-
-const deleteCluster = async r => Promise.resolve(r);
 
 const deleteRespository = async r => new ECR({
     accessKeyId: process.env.ACCESS_KEY_ID,
