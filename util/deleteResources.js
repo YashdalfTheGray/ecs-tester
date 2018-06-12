0a8eaea81fd03b5754f676a81e6f8c4ea022d76f
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2018-06-12 07:07:53 -0700

    wired up cluster delete using cluster-cleanup

--- util/deleteResources.js
+++ util/deleteResources.js
@@ -1,4 +1,6 @@
-const { getEcrClient, getEcsClient } = require('.');
+const { ClusterCleanup } = require('cluster-cleanup');
+
+const { getEcrClient } = require('.');
 
 module.exports = async (manifest) => {
     const { ACCESS_KEY_ID, SECRET_ACCESS_KEY } = process.env;
@@ -22,20 +24,23 @@ module.exports = async (manifest) => {
 };
 
 const deleteCluster = async (r) => {
-    const ecs = getEcsClient();
+    const cleanup = new ClusterCleanup({
+        accessKeyId: process.env.ACCESS_KEY_ID,
+        secretAccessKey: process.env.SECRET_ACCESS_KEY,
+        region: process.env.REGION,
+        enableFargate: process.env.USE_FARGATE
+    });
 
-    // 1. find CloudFormation stack
-    // 2. find all services
-    // 3. batch scale all services down to 0
-    // 4. find all container instances
-    // 5. deregister all container instances
-    // 6. find all services, again
-    // 7. delete all services
-    // 8. delete CloudFormation stack
-    // 9. poll CloudFormation until stack deleted
-    // 10. delete cluster
+    const events = cleanup.deleteClusterAndResources(r.name, { verbose: true });
 
-    return Promise.resolve(r);
+    return new Promise((resolve, reject) => {
+        events.onDone((clusterName) => {
+            resolve(clusterName);
+        });
+        events.onError((error) => {
+            reject(error);
+        });
+    });
 };
 
 const deleteRespository = async r =>
