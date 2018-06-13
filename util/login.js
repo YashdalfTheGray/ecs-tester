a96c19feeee8def37071941e5a6383a43d40b789
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2018-06-13 06:37:12 -0700

    click somewhere else after input in textbox just to be extra safe

--- util/login.js
+++ util/login.js
@@ -6,12 +6,15 @@ module.exports = async (browser, url) => {
     await page.goto(url);
     await page.waitForSelector('#resolver_container #resolving_input');
     await page.type('#resolver_container #resolving_input', AWS_ACCOUNT);
+    await page.click('b.awsui-text-normal');
     await page.click('button#next_button');
 
     // IAM account login page
     await page.waitForSelector('#accountFields #username');
     await page.type('#accountFields #username', IAM_USERNAME);
+    document.querySelector('#accountFields label');
     await page.type('#accountFields #password', IAM_PASSWORD);
+    document.querySelector('#accountFields label');
     await page.click('a#signin_button');
 
     await page.setViewport({ height: 768, width: 1200 });
